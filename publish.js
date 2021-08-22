730a195095040e9bbb4c5d3fc4eefc39cd98d025
Tim Lindgren <timothy.lindgren@bc.edu>
2021-08-22 13:50:21 -0400

    Adding some comments

--- publish.js
+++ publish.js
@@ -1,6 +1,8 @@
+// === Custom JS for Obsidian Publish - Tim Lindgren ===
 
+// Adding top navigation elements - a little hacky since it reloads the site, but will do for now
 var sitenav = document.createElement("nav");
 document.querySelector(".site-header").appendChild(sitenav);
 
 sitenav.innerHTML = '<a href="https://timlindgren.com/About">About</a><a href="https://timlindgren.com/Now">Now</a><a href="https://timlindgren.com/Portfolio">Portfolio</a><a href="https://timlindgren.com/Notes+Overview">Notes</a><a href="https://timlindgren.com/Changelog">Changelog</a>';
-console.log('updating'); 
+ 
